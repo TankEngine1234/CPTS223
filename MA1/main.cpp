6053c5e6f1b5f50541fa62c039ce48610797f412
Nihal Thomas <joethomas3657@gmail.com>
2023-09-17 22:42:17 -0700

    Add files via upload

--- MA1/main.cpp
+++ MA1/main.cpp
@@ -1,3 +1,27 @@
+/*
+  Poor Attributes in the Code:
+
+  1. Global Variables: The use of #define to declare global constants (like SIZE) can lead to issues 
+     as they don't have any scope. Using const or constexpr variables would be a better choice.
+
+  2. Error Handling: The error handling in the code is not robust. Terminating the program 
+     immediately upon encountering an error (like queue overflow or underflow) is not a good practice. 
+     It would be better to handle errors gracefully, possibly by returning error codes or throwing exceptions.
+
+  3. Lack of Modularity: The main.cpp file contains both the class definition and implementation, 
+     which makes the code less modular and harder to maintain. Separating the class definition 
+     (in a header file) and implementation (in a source file) would be a better approach.
+
+  4. Inefficient Data Structure: The queue is implemented using an array, which can lead to inefficient 
+     operations, especially for large queues. Using a linked list or a circular buffer could be a more 
+     efficient choice.
+
+  5. Hardcoded Values: The code contains hardcoded values (like the error messages and exit codes), 
+     which can make the code less flexible and harder to maintain. Using named constants or 
+     configuration files to store these values would be a better approach.
+
+*/
+
 #include "queue.h"
 #include "testQueue.h"
 
